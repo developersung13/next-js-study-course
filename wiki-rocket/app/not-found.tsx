b2a636c41d887d042762bf2cc0d54c9193620d0c
developersung13 <developersung13@gmail.com>
2023-04-10 23:39:54 +0900

    Add (WikiRocket) project file

---
+++ wiki-rocket/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div className='bg-slate-200 mx-auto max-w-lg p-1 min-h-screen'>
+      <h1 className='m-4 text-2xl font-bold'>404 - Page Not Found</h1>
+      <p className='m-4'>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
