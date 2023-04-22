3800557b4dceba03785802a1d8f75b6bea1247f2
developersung13 <developersung13@gmail.com>
2023-04-22 10:25:49 +0900

    Add (coding-apple-nextjs-course) project file

---
+++ coding-apple-nextjs-course/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className='flex justify-center mt-[20rem] font-thin'>
+      <div className='text-left'>
+        <h1 className='text-[4rem] tracking-[2rem]'>LONG SHOP</h1>
+        <h3 className='text-[1.5rem] tracking-[0.5rem] underline underline-offset-8 decoration-solid decoration-1'>
+          MORE THAN SOPHISTICATED
+        </h3>
+      </div>
+    </div>
+  );
+}
