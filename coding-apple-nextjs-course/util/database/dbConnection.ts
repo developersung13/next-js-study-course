cdd58e81806c8fdf9482c93fdf9a76926cce062c
developersung13 <developersung13@gmail.com>
2023-05-07 02:05:14 +0900

    Add duplicate connection filter code about MongoDB

--- coding-apple-nextjs-course/util/database/dbConnection.ts
+++ coding-apple-nextjs-course/util/database/dbConnection.ts
@@ -1,9 +1,21 @@
-import { MongoClient } from 'mongodb';
+import { ConnectOptions, MongoClient } from 'mongodb';
 
+const options = { useNewUrlParser: true } as ConnectOptions;
 let connectDB: Promise<MongoClient>;
-connectDB = new MongoClient(
-  process.env.MONGODB_URI as string,
-  undefined
-).connect();
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(
+      process.env.MONGODB_URI as string,
+      options
+    ).connect();
+  }
+  connectDB = global._mongo;
+} else {
+  connectDB = new MongoClient(
+    process.env.MONGODB_URI as string,
+    options
+  ).connect();
+}
 
 export { connectDB };
