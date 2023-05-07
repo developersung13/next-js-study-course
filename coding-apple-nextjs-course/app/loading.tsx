c99e6ac6a6ca0b334c9c1b8fd8300640b130cc4f
developersung13 <developersung13@gmail.com>
2023-05-07 11:06:16 +0900

    Modify 'Loading' component

--- coding-apple-nextjs-course/app/loading.tsx
+++ coding-apple-nextjs-course/app/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <h1 className='text-center text-[4rem]'>Loading...</h1>;
+  return (
+    <h1 className='w-screen h-screen flex justify-center items-center text-3xl font-thin'>
+      Loading...
+    </h1>
+  );
 }
